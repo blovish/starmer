4e5f903d81007cad91271d50e1826596e3d40acc
jamesmgittins <jamesmgittins@gmail.com>
2026-04-22 22:06:48 +0100

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'starmer-survivors-v1';
+
+const PRECACHE = [
+  '/',
+  '/images/starmer.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached ?? fetch(event.request))
+  );
+});
